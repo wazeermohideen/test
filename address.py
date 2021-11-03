52dd32eb2e7cf26fff9fd869743d88e8dd179360
Wazeer Mohideen <92957191+wazeermohideen@users.noreply.github.com>
2021-11-03 08:44:04 -0400

    test commit

---
+++ address.py
@@ -0,0 +1,7 @@
+class Adress:
+    def __init__(self, country, city, state, postal):
+        self.country = country
+        self.state = state
+        self.city = city
+        self.postal = postal
+        
---
+++ person.py
@@ -0,0 +1,11 @@
+from address import Adress
+
+class Person:
+    def __init__(self, first, last, dateOfBirth, phone, address):
+        self.first_name = first
+        self.last_name = last
+        self.date_of_birth = dateOfBirth
+        self.phone = phone
+        self.adresses = []
+
+Person
